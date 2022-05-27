8ff402a2d1c65634b8582226a74302bbf484b7e5
Alex Lockhart <alex@lockhart.dev>
2022-05-27 11:55:59 -0300

    updates

--- src/get-emulators.js
+++ src/get-emulators.js
@@ -2,20 +2,10 @@ const alfy = require("alfy");
 const util = require("util");
 const child_process = require("child_process");
 
-const CACHE_KEY = "avds";
-
 exec = util.promisify(child_process.exec);
 
-const getEmulators = exec("emulator -list-avds")
+exec("emulator -list-avds")
   .then((out) => out.stdout)
-  .then((avds) => {
-    alfy.cache.set(CACHE_KEY, avds);
-    return avds;
-  });
-
-const cachedEmulators = alfy.cache.get(CACHE_KEY);
-
-(cachedEmulators ? Promise.resolve(cachedEmulators) : getEmulators)
   .then((avds) =>
     avds
       .split("\n")
--- src/get-simulators.js
+++ src/get-simulators.js
@@ -1,21 +1,28 @@
 const alfy = require("alfy");
 const util = require("util");
 const child_process = require("child_process");
-
-const CACHE_KEY = "sims";
+const fs = require("fs");
+const path = require("path");
 
 exec = util.promisify(child_process.exec);
+writeFile = util.promisify(fs.writeFile);
+readFile = util.promisify(fs.readFile);
 
-const getSimulators = exec("xcrun simctl list --json devices available")
-  .then((out) => out.stdout)
-  .then((sims) => {
-    alfy.cache.set(CACHE_KEY, sims);
-    return sims;
-  });
+const cachePath = path.join(__dirname, "../cache/sims.json");
 
-const cachedSimulators = alfy.cache.get(CACHE_KEY);
+const getSimulators = () =>
+  exec("xcrun simctl list --json devices available")
+    .then((out) => out.stdout)
+    .then(async (sims) => {
+      // await writeFile(cachePath, sims);
+      // const buffer = await readFile(cachePath);
+      // const str = buffer.toString();
+      // console.log(str);
+      // return JSON.parse(str);
+      return sims;
+    });
 
-(cachedSimulators ? Promise.resolve(cachedSimulators) : getSimulators)
+getSimulators()
   .then(JSON.parse)
   .then((details) => details.devices)
   .then((runtimes) =>
@@ -37,3 +44,53 @@ const cachedSimulators = alfy.cache.get(CACHE_KEY);
   .then((deviceses) => deviceses.filter((devices) => !!devices))
   .then((devices) => devices.flat())
   .then((items) => alfy.output(items));
+
+/**
+const alfy = require("alfy");
+const util = require("util");
+const child_process = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+exec = util.promisify(child_process.exec);
+writeFile = util.promisify(fs.writeFile);
+readFile = util.promisify(fs.readFile);
+
+const cachePath = path.join(__dirname, "../cache/sims.json");
+
+const getSimulators = () =>
+  exec("xcrun simctl list --json devices available")
+    .then((out) => out.stdout)
+    .then(async (sims) => {
+      // await writeFile(cachePath, sims);
+      return sims;
+    });
+
+Promise.race([
+  // readFile(cachePath).then((buffer) => buffer.toString()),
+  getSimulators(),
+])
+  .then(console.log)
+  .then(JSON.parse)
+  .then((details) => details.devices)
+  .then((runtimes) =>
+    Object.keys(runtimes).map((runtime) => {
+      const matches = /com.apple.CoreSimulator.SimRuntime.iOS-(.*)/.exec(
+        runtime
+      );
+
+      if (!matches) return null;
+
+      const version = matches[1].replace("-", ".");
+      return runtimes[runtime].map((device) => ({
+        title: device.name,
+        subtitle: version,
+        arg: device.udid,
+      }));
+    })
+  )
+  .then((deviceses) => deviceses.filter((devices) => !!devices))
+  .then((devices) => devices.flat())
+  .then((items) => alfy.output(items));
+
+   */
